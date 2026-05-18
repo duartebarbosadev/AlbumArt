7119d3569e95827a987495fb619b75a6483d84a4
Duarte Barbosa <duartebarbosapt@gmail.com>
2026-05-18 17:01:30 +1000

    chore(reliability): Implement strict mode for debug variant

--- app/src/main/java/com/sample/albumart/AlbumArtApplication.kt
+++ app/src/main/java/com/sample/albumart/AlbumArtApplication.kt
@@ -1,6 +1,9 @@
 package com.sample.albumart
 
 import android.app.Application
+import android.content.pm.ApplicationInfo
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy.Builder
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -9,6 +12,28 @@ class AlbumArtApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        // TODO use strict mode
+        setStrictModePolicy()
+    }
+
+
+    /**
+     * Return true if the application is debuggable.
+     */
+    private fun isDebuggable(): Boolean {
+        return 0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE
+    }
+
+    /**
+     * Set a thread policy that detects all potential problems on the main thread, such as network
+     * and disk access.
+     *
+     * If a problem is found, the offending call will be logged and the application will be killed.
+     */
+    private fun setStrictModePolicy() {
+        if (isDebuggable()) {
+            StrictMode.setThreadPolicy(
+                Builder().detectAll().penaltyLog().build(),
+            )
+        }
     }
 }
