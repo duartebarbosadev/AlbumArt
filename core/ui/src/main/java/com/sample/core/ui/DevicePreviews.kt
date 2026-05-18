ff4eb70e10949ab9da0e082b706c50b9c6b9b5e0
Duarte Barbosa <duartebarbosapt@gmail.com>
2026-05-18 18:46:07 +1000

    refactor(previews): remove desktop preview from DevicePreviews annotation

--- core/ui/src/main/java/com/sample/core/ui/DevicePreviews.kt
+++ core/ui/src/main/java/com/sample/core/ui/DevicePreviews.kt
@@ -7,5 +7,4 @@ import androidx.compose.ui.tooling.preview.Preview
 @Preview(name = "phone_in_landscape", widthDp = 891, heightDp = 411, showBackground = true)
 @Preview(name = "foldable", device = Devices.FOLDABLE, showBackground = true)
 @Preview(name = "tablet", device = Devices.TABLET, showBackground = true)
-@Preview(name = "desktop", device = Devices.DESKTOP, showBackground = true)
 annotation class DevicePreviews
